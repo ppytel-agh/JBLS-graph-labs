cbb690a555a942319ab7d1cb86cd90e4be9f36d4
Paweł Pytel <ppytel@student.agh.edu.pl>
2023-05-10 11:12:26 +0200

    added optional outputing of graph generated from graphic sequence

--- check-if-sequence-is-graphic-and-construct-graph-if-is.py
+++ check-if-sequence-is-graphic-and-construct-graph-if-is.py
@@ -1,4 +1,6 @@
 import argparse
+import os
+from networkx.drawing.nx_pydot import write_dot
 
 from graphic_sequence import is_sequence_graphic
 from graphic_sequence import create_graph_from_sequence
@@ -7,12 +9,20 @@ from script_common import draw_graph_on_circle
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('potential_sequence', type=int, nargs="+", help='sequence_to_check')
+    parser.add_argument('--output_name', type=str, help='name of dot file to output generated graph')
     args = parser.parse_args()
 
     if is_sequence_graphic(args.potential_sequence):
         number_of_nodes = len(args.potential_sequence)
         node_identifiers = list(range(1, number_of_nodes + 1))
         nx_graph = create_graph_from_sequence(args.potential_sequence, node_identifiers)
+
+        if args.output_name:
+            filename = f'{args.output_name}.dot'
+            output_path = os.path.join(os.getcwd(), filename)
+
+            write_dot(nx_graph, output_path)
+
         draw_graph_on_circle(nx_graph)
     else:
         print("sequence is not graphic")
--- readme.md
+++ readme.md
@@ -6,6 +6,28 @@ z1 - encode-as-adjacency-matrix.py, encode-as-incidence-matrix.py, encode-as-adj
 z2 - draw-in-circle.py
 z3 - draw-random-graph-with-number-of-edges.py, draw-random-graph-with-edge-probability.py
 
+zestaw 2
+
+z1
+check-if-sequence-is-graphic-and-construct-graph-if-is.py <ranks+> [output dot file name]
+
+$python3 check-if-sequence-is-graphic-and-construct-graph-if-is.py 4 2 2 3 2 1 4 2 2 2 2 --output_name graph_for_randomization
+valid sequence, generates graph and writes it out
+
+$python3 check-if-sequence-is-graphic-and-construct-graph-if-is.py 4 4 3 1 2
+invalid sequence
+
+z2
+randomize-graph-edges.py <path to dot file> <number of swaps>
+
+$python3 randomize-graph-edges.py ./graph_for_randomization.dot 10
+swaps 10 edges, persists vertice ranks
+
+z3
+find-giant-component.py <path to dot file>
+
+$pyhon3 find-giant-component.py ./graph_for_randomization.dot
+
 MODUŁY
 
 script_common.py - funkcje do obsługi obiektów nxgraph
